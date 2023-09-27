37c171c34837584335032c2de800057c349df1c3
Alex Mickelson <alexmickelson96@gmail.com>
2023-09-27 13:00:16 -0600

    working on serialization

--- Management/Models/Local/LocalModules.cs
+++ Management/Models/Local/LocalModules.cs
@@ -3,12 +3,12 @@ namespace LocalModels;
 public record LocalModule
 {
   public string Name { get; init; } = string.Empty;
-  public string Id { get; init; } = string.Empty;
+  public string Id { get; init; } = DateTime.UtcNow.Ticks.ToString();
+  public ulong? CanvasId { get; set; } = null;
+  public string Notes { get; set; } = string.Empty;
   public IEnumerable<LocalAssignment> Assignments { get; init; } =
     Enumerable.Empty<LocalAssignment>();
 
   public IEnumerable<LocalQuiz> Quizzes { get; init; } = Enumerable.Empty<LocalQuiz>();
 
-  public ulong? CanvasId { get; set; } = null;
-  public string Notes { get; set; } = string.Empty;
 }
