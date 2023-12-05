5528d79bb2040bba7f9b40070555a2b06a968b54
Alex Mickelson <alexmickelson96@gmail.com>
2023-12-05 16:41:07 -0700

    version bump

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-VERSION="1.0"
+VERSION="1.1"
 
 dotnet publish Management.Web/ \
     --os linux \
@@ -11,10 +11,10 @@ dotnet publish Management.Web/ \
     -p ContainerImageTags="\"$VERSION;latest\"" \
     -p ContainerRepository="canvas_management"
 
-docker image tag canvas_management:$VERSION alexmickelson/canvas_management:$VERSION
-docker image tag canvas_management:latest alexmickelson/canvas_management:latest
 
 
 echo "to push run: "
+echo "docker image tag canvas_management:$VERSION alexmickelson/canvas_management:$VERSION"
+echo "docker image tag canvas_management:latest alexmickelson/canvas_management:latest"
 echo "docker push alexmickelson/canvas_management:$VERSION"
 echo "docker push alexmickelson/canvas_management:latest"
