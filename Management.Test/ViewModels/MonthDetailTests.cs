0e3e94ff653e6e9c1c2a50463390d6361daf8acf
Alex Mickelson <alexmickelson96@gmail.com>
2024-02-07 16:27:20 -0700

    hooks

--- Management.Test/ViewModels/MonthDetailTests.cs
+++ Management.Test/ViewModels/MonthDetailTests.cs
@@ -6,10 +6,14 @@ public class MonthDetailTests
   public void TestCanGetMonthName()
   {
     var calendarMonth = new CalendarMonth(2022, 2);
+
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
     var detail = new MonthDetail()
     {
       Month = calendarMonth
     };
+#pragma warning restore BL0005 // Component parameter should not be set outside of its component.
+
 
     detail.MonthName.Should().Be("February");
   }
