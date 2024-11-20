2d673b0fd803f1cde26778aac37c95f7c3762b98
Alex Mickelson <alexmickelson96@gmail.com>
2024-11-20 13:43:44 -0700

    do not reset settings when server changed

--- nextjs/src/app/course/[courseName]/settings/AllSettings.tsx
+++ nextjs/src/app/course/[courseName]/settings/AllSettings.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useLocalCourseSettingsQuery } from "@/hooks/localCourse/localCoursesHooks";
 import AssignmentGroupManagement from "./AssignmentGroupManagement";
 import DaysOfWeekSettings from "./DaysOfWeekSettings";
 import DefaultDueTime from "./DefaultDueTime";
@@ -9,12 +8,10 @@ import HolidayConfig from "./HolidayConfig";
 import SettingsHeader from "./SettingsHeader";
 import StartAndEndDate from "./StartAndEndDate";
 import SubmissionDefaults from "./SubmissionDefaults";
-import { Fragment } from "react";
 
 export default function AllSettings() {
-  const [_, { dataUpdatedAt }] = useLocalCourseSettingsQuery();
   return (
-    <Fragment key={dataUpdatedAt}>
+    <>
       <SettingsHeader />
       <DaysOfWeekSettings />
       <StartAndEndDate />
@@ -23,6 +20,6 @@ export default function AllSettings() {
       <DefaultDueTime />
       <AssignmentGroupManagement />
       <HolidayConfig />
-    </Fragment>
+    </>
   );
 }
